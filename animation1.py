d714ac71c9a9edacaca2eaafd677cc9ab8f137c5
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-09 19:48:13 +0530

    Update animation1.py

--- animation1.py
+++ animation1.py
@@ -3,7 +3,7 @@ from collections import deque
 
 from DaisyX import ALIVE_NAME, CMD_HELP
 
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from DaisyX.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Daisy"
 
