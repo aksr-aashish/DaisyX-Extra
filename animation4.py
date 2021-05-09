8f183c00b8741da726c21cc40585f9ca852448b5
Inuka Asith <inukaasith7@gmail.com>
2021-05-09 20:15:06 +0530

    Update animation4.py

--- animation4.py
+++ animation4.py
@@ -4,7 +4,7 @@ import asyncio
 
 from DaisyX import CMD_HELP
 
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from DaisyX.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @bot.on(admin_cmd(pattern=f"loding$", outgoing=True))
